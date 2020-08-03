f8d014afbf206a231ca929b4d2250453eefbcdb4
Colin HIll <colin@projecteva.net>
2020-08-03 14:47:10 -0600

    Fixes

--- MetadataDBWork/Program.cs
+++ MetadataDBWork/Program.cs
@@ -8,6 +8,8 @@ using MusicLibraryTools;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.Linq;
+using System.Threading;
+using System.Transactions;
 
 namespace MetadataDBWork
 {
@@ -21,7 +23,7 @@ namespace MetadataDBWork
             "CREATE TABLE AlbumArtists (ID INTEGER PRIMARY KEY, Name TEXT UNIQUE);\r\n" +
             "CREATE TABLE Albums (ID INTEGER PRIMARY KEY, AlbumArtistID INTEGER NOT NULL REFERENCES AlbumArtists (ID), Name TEXT NOT NULL, Path TEXT NOT NULL);\r\n" +
             "CREATE TABLE Artists (ID INTEGER PRIMARY KEY, Name TEXT UNIQUE);\r\n" +
-            "CREATE TABLE Files (ID INTEGER PRIMARY KEY, Path TEXT UNIQUE, ScanTime DATETIME NOT NULL, ArtistID INTEGER REFERENCES Artists (ID), AlbumID INTEGER REFERENCES Albums (ID), CodecName TEXT NOT NULL, CodecType TEXT NOT NULL, AverageBitrate INTEGER NOT NULL, MaxBitrate INTEGER NOT NULL, BitsPerSample INTEGER NOT NULL, SampleRate INTEGER NOT NULL, Channels INTEGER NOT NULL, DurationInFrames INTEGER NOT NULL);\r\n" +
+            "CREATE TABLE Files (ID INTEGER PRIMARY KEY, \"Set\" INTEGER NOT NULL, Path TEXT UNIQUE, ScanTime DATETIME NOT NULL, ArtistID INTEGER REFERENCES Artists (ID), AlbumID INTEGER REFERENCES Albums (ID), CodecName TEXT NOT NULL, CodecType TEXT NOT NULL, AverageBitrate INTEGER NOT NULL, MaxBitrate INTEGER NOT NULL, BitsPerSample INTEGER NOT NULL, SampleRate INTEGER NOT NULL, Channels INTEGER NOT NULL, DurationInFrames INTEGER NOT NULL);\r\n" +
             "CREATE TABLE Metadata (ID INTEGER PRIMARY KEY, FileID INTEGER REFERENCES Files (ID) NOT NULL, \"Key\" TEXT NOT NULL, Value TEXT NOT NULL);\r\n" +
             "COMMIT TRANSACTION;\r\n" +
             "PRAGMA foreign_keys = on\r\n";
@@ -30,94 +32,153 @@ namespace MetadataDBWork
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            File.Delete("cache.db");
-            using var conn = new SQLiteConnection("URI=file:cache.db");
+            bool createtables = !File.Exists("cache.db");
+            using var conn = new SQLiteConnection("URI=file:cache.db;DateTimeKind=Utc");
             conn.Open();
 
-            using var command = conn.CreateCommand();
-            command.CommandText = creationsql_;
-            command.ExecuteNonQuery();
-
-            string[] paths = new string[]
+            if (createtables)
             {
-                @"\\ritsuko.projecteva.net\Roon\Purchased Sync",
-                @"\\ritsuko.projecteva.net\AllMusic\FLAC",
-                @"\\ritsuko.projecteva.net\AllMusic\FLAC2",
-                @"\\ritsuko.projecteva.net\AllMusic\HiRes",
+                using var command = conn.CreateCommand();
+                command.CommandText = creationsql_;
+                command.ExecuteNonQuery();
+            }
+
+            var paths = new []
+            {
+                ( @"\\ritsuko.projecteva.net\Roon\Purchased Sync", 30 ),
+                ( @"\\ritsuko.projecteva.net\AllMusic\FLAC", 1 ),
+                ( @"\\ritsuko.projecteva.net\AllMusic\FLAC2", 2 ),
+                ( @"\\ritsuko.projecteva.net\AllMusic\HiRes\Stereo", 10 ),
+                ( @"\\ritsuko.projecteva.net\AllMusic\HiRes\Multi", 20 )
             };
+
+            var filesdict = new ConcurrentDictionary<string, ValueTuple<int, int, DateTime>>();
+
+            using var getfilescomm = conn.CreateCommand();
+            getfilescomm.CommandText = "SELECT ID, \"Set\", Path, ScanTime FROM Files";
+            using (var reader = getfilescomm.ExecuteReader())
+                while (reader.Read())
+                    filesdict[reader.GetString(2)] = (reader.GetInt32(0), reader.GetInt32(1), DateTime.SpecifyKind(reader.GetDateTime(3), DateTimeKind.Utc));
 
             foreach (var scanpath in paths)
             {
                 GC.Collect();
 
-                DirectoryInfo di = new DirectoryInfo(scanpath);
+                DirectoryInfo di = new DirectoryInfo(scanpath.Item1);
+                int scanset = scanpath.Item2;
                 var files = di.EnumerateFileSystemInfos("*", SearchOption.AllDirectories).Where(fsi => ValidExtensions.Contains(Path.GetExtension(fsi.FullName).ToLower()) && ((fsi.Attributes & FileAttributes.Directory) == 0)).ToArray();
 
                 var bag = new ConcurrentBag<Tuple<string, DateTime, IMetadataProvider>>();
+                var deletekeys = new ConcurrentBag<int>();
 
-                Parallel.ForEach(files, (fi) => { bag.Add(new Tuple<string, DateTime, IMetadataProvider>(fi.FullName, DateTime.UtcNow, Metadata.GetProvider(fi.FullName))); });
-
-                using var filescomm = conn.CreateCommand();
-                var pathparam = filescomm.Parameters.Add("@Path", System.Data.DbType.String);
-                var scantimeparam = filescomm.Parameters.Add("@ScanTime", System.Data.DbType.DateTime);
-                var codecnameparam = filescomm.Parameters.Add("@CodecName", System.Data.DbType.String);
-                var codectypeparam = filescomm.Parameters.Add("@CodecType", System.Data.DbType.String);
-                var averagebitrateparam = filescomm.Parameters.Add("@AverageBitrate", System.Data.DbType.Int32);
-                var maxbitrateparam = filescomm.Parameters.Add("@MaxBitrate", System.Data.DbType.Int32);
-                var bitspersampleparam = filescomm.Parameters.Add("@BitsPerSample", System.Data.DbType.Int32);
-                var samplerateparam = filescomm.Parameters.Add("@SampleRate", System.Data.DbType.Int32);
-                var channelsparam = filescomm.Parameters.Add("@Channels", System.Data.DbType.Int32);
-                var durationinframesparam = filescomm.Parameters.Add("@DurationInFrames", System.Data.DbType.Int32);
-                filescomm.CommandText = "INSERT INTO Files (Path, ScanTime, CodecName, CodecType, AverageBitrate, MaxBitrate, BitsPerSample, SampleRate, Channels, DurationInFrames)" +
-                " VALUES (@Path, @ScanTime, @CodecName, @CodecType, @AverageBitrate, @MaxBitrate, @BitsPerSample, @SampleRate, @Channels, @DurationInFrames);";
-
-                using (var transaction = conn.BeginTransaction())
+                Parallel.ForEach(files, (fi) =>
                 {
-                    foreach (var file in bag)
+                    bool scan = false;
+                    if (filesdict.ContainsKey(fi.FullName))
                     {
-                        string path = file.Item1;
-                        IMetadataProvider mp = file.Item3;
-                        ICodecProvider cp = mp as ICodecProvider;
-                        pathparam.Value = path;
-                        scantimeparam.Value = file.Item2;
-                        codecnameparam.Value = cp.CodecName;
-                        codectypeparam.Value = cp.CodecType.ToString();
-                        averagebitrateparam.Value = cp.AverageBitrate;
-                        maxbitrateparam.Value = cp.MaxBitrate;
-                        bitspersampleparam.Value = cp.BitsPerSample;
-                        samplerateparam.Value = cp.Samplerate;
-                        channelsparam.Value = cp.Channels;
-                        durationinframesparam.Value = cp.DurationInFrames;
-                        filescomm.ExecuteNonQuery();
-                    }
-                    transaction.Commit();
-                }
-
-                var keymap = new Dictionary<string, int>();
-                command.CommandText = "SELECT ID, Path FROM Files";
-                using (var reader = command.ExecuteReader())
-                    while (reader.Read())
-                        keymap.Add(reader.GetString(1), reader.GetInt32(0));
-
-                using var metacomm = conn.CreateCommand();
-                metacomm.CommandText = "INSERT INTO Metadata (FileID, \"Key\", Value) VALUES (@FileID, @Key, @Value)";
-                var fileidparam = metacomm.Parameters.Add("@FileID", System.Data.DbType.Int32);
-                var keyparam = metacomm.Parameters.Add("@Key", System.Data.DbType.String);
-                var valueparam = metacomm.Parameters.Add("@Value", System.Data.DbType.String);
-
-                using (var transaction = conn.BeginTransaction())
-                {
-                    foreach (var file in bag)
-                    {
-                        fileidparam.Value = keymap[file.Item1];
-                        foreach (var kv in file.Item3.GetTextMetadata())
+                        var file = filesdict[fi.FullName];
+                        if (fi.LastWriteTimeUtc > file.Item3)
                         {
-                            keyparam.Value = kv.Key;
-                            valueparam.Value = kv.Value;
-                            metacomm.ExecuteNonQuery();
+                            deletekeys.Add(file.Item1);
+                            scan = true;
                         }
                     }
-                    transaction.Commit();
+                    else
+                        scan = true;
+                    if (scan)
+                        bag.Add(new Tuple<string, DateTime, IMetadataProvider>(fi.FullName, DateTime.UtcNow, Metadata.GetProvider(fi.FullName)));
+                });
+
+                if (!deletekeys.IsEmpty)
+                {
+                    using var deletecomm = conn.CreateCommand();
+                    var idparam = deletecomm.Parameters.Add("@ID", System.Data.DbType.Int32);
+                    using (var transaction = conn.BeginTransaction())
+                    {
+                        deletecomm.CommandText = "DELETE FROM Metadata WHERE FileID = @ID";
+                        foreach (var id in deletekeys)
+                        {
+                            idparam.Value = id;
+                            deletecomm.ExecuteNonQuery();
+                        }
+                        deletecomm.CommandText = "DELETE FROM Files WHERE ID = @ID";
+                        foreach (var id in deletekeys)
+                        {
+                            idparam.Value = id;
+                            deletecomm.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+                    }
+                }
+
+                if (!bag.IsEmpty)
+                {
+                    using var filescomm = conn.CreateCommand();
+                    filescomm.Parameters.AddWithValue("@Set", scanset);
+                    var pathparam = filescomm.Parameters.Add("@Path", System.Data.DbType.String);
+                    var scantimeparam = filescomm.Parameters.Add("@ScanTime", System.Data.DbType.DateTime);
+                    var codecnameparam = filescomm.Parameters.Add("@CodecName", System.Data.DbType.String);
+                    var codectypeparam = filescomm.Parameters.Add("@CodecType", System.Data.DbType.String);
+                    var averagebitrateparam = filescomm.Parameters.Add("@AverageBitrate", System.Data.DbType.Int32);
+                    var maxbitrateparam = filescomm.Parameters.Add("@MaxBitrate", System.Data.DbType.Int32);
+                    var bitspersampleparam = filescomm.Parameters.Add("@BitsPerSample", System.Data.DbType.Int32);
+                    var samplerateparam = filescomm.Parameters.Add("@SampleRate", System.Data.DbType.Int32);
+                    var channelsparam = filescomm.Parameters.Add("@Channels", System.Data.DbType.Int32);
+                    var durationinframesparam = filescomm.Parameters.Add("@DurationInFrames", System.Data.DbType.Int32);
+                    filescomm.CommandText = "INSERT INTO Files (Path, \"Set\", ScanTime, CodecName, CodecType, AverageBitrate, MaxBitrate, BitsPerSample, SampleRate, Channels, DurationInFrames)" +
+                    " VALUES (@Path, @Set, @ScanTime, @CodecName, @CodecType, @AverageBitrate, @MaxBitrate, @BitsPerSample, @SampleRate, @Channels, @DurationInFrames);";
+
+                    using (var transaction = conn.BeginTransaction())
+                    {
+                        foreach (var file in bag)
+                        {
+                            string path = file.Item1;
+                            IMetadataProvider mp = file.Item3;
+                            ICodecProvider cp = mp as ICodecProvider;
+                            pathparam.Value = path;
+                            scantimeparam.Value = file.Item2;
+                            codecnameparam.Value = cp.CodecName;
+                            codectypeparam.Value = cp.CodecType.ToString();
+                            averagebitrateparam.Value = cp.AverageBitrate;
+                            maxbitrateparam.Value = cp.MaxBitrate;
+                            bitspersampleparam.Value = cp.BitsPerSample;
+                            samplerateparam.Value = cp.Samplerate;
+                            channelsparam.Value = cp.Channels;
+                            durationinframesparam.Value = cp.DurationInFrames;
+                            filescomm.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+                    }
+
+                    var keymap = new Dictionary<string, int>();
+                    using (var query = conn.CreateCommand())
+                    {
+                        query.CommandText = "SELECT ID, Path FROM Files";
+                        using (var reader = query.ExecuteReader())
+                            while (reader.Read())
+                                keymap.Add(reader.GetString(1), reader.GetInt32(0));
+                    }
+
+                    using var metacomm = conn.CreateCommand();
+                    metacomm.CommandText = "INSERT INTO Metadata (FileID, \"Key\", Value) VALUES (@FileID, @Key, @Value)";
+                    var fileidparam = metacomm.Parameters.Add("@FileID", System.Data.DbType.Int32);
+                    var keyparam = metacomm.Parameters.Add("@Key", System.Data.DbType.String);
+                    var valueparam = metacomm.Parameters.Add("@Value", System.Data.DbType.String);
+
+                    using (var transaction = conn.BeginTransaction())
+                    {
+                        foreach (var file in bag)
+                        {
+                            fileidparam.Value = keymap[file.Item1];
+                            foreach (var kv in file.Item3.GetTextMetadata())
+                            {
+                                keyparam.Value = kv.Key;
+                                valueparam.Value = kv.Value;
+                                metacomm.ExecuteNonQuery();
+                            }
+                        }
+                        transaction.Commit();
+                    }
                 }
             }
 
