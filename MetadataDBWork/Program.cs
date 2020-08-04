1cf86a73dfbcbe112f45ce82d3ee34c367289831
Colin HIll <colin@projecteva.net>
2020-08-04 12:35:58 -0600

    Database creation complete except for artwork

--- MetadataDBWork/Program.cs
+++ MetadataDBWork/Program.cs
@@ -10,6 +10,7 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Threading;
 using System.Transactions;
+using System.Runtime.InteropServices;
 
 namespace MetadataDBWork
 {
@@ -25,6 +26,16 @@ namespace MetadataDBWork
             "CREATE TABLE Artists (ID INTEGER PRIMARY KEY, Name TEXT UNIQUE);\r\n" +
             "CREATE TABLE Files (ID INTEGER PRIMARY KEY, \"Set\" INTEGER NOT NULL, Path TEXT UNIQUE, ScanTime DATETIME NOT NULL, ArtistID INTEGER REFERENCES Artists (ID), AlbumID INTEGER REFERENCES Albums (ID), CodecName TEXT NOT NULL, CodecType TEXT NOT NULL, AverageBitrate INTEGER NOT NULL, MaxBitrate INTEGER NOT NULL, BitsPerSample INTEGER NOT NULL, SampleRate INTEGER NOT NULL, Channels INTEGER NOT NULL, DurationInFrames INTEGER NOT NULL);\r\n" +
             "CREATE TABLE Metadata (ID INTEGER PRIMARY KEY, FileID INTEGER REFERENCES Files (ID) NOT NULL, \"Key\" TEXT NOT NULL, Value TEXT NOT NULL);\r\n" +
+            "CREATE INDEX FileIDIndex ON Metadata(FileID ASC);\r\n" +
+            "CREATE INDEX AlbumsAlbumArtistIDIndex ON Albums (AlbumArtistID ASC);\r\n" +
+            "CREATE VIEW MetadataMapView AS SELECT *,\r\n"+
+            "(SELECT Value FROM Metadata WHERE FileID = Files.ID AND \"Key\" = 'ARTIST') AS Artist,\r\n" +
+            "COALESCE(\r\n" +
+            "   (SELECT Value FROM Metadata WHERE FileID = Files.ID AND \"Key\" = 'ALBUMARTIST'),\r\n" +
+            "   (SELECT Value FROM Metadata WHERE FileID = Files.ID AND \"Key\" = 'ARTIST')) AS AlbumArtist,\r\n" +
+            "   (SELECT Value FROM Metadata WHERE FileID = Files.ID AND \"Key\" = 'ALBUM') As Album,\r\n" +
+            "   (SELECT Value FROM Metadata WHERE FileID = Files.ID AND \"Key\" = 'TRACKNUMBER') As TrackNumber\r\n" +
+            "   FROM Files;\r\n" +
             "COMMIT TRANSACTION;\r\n" +
             "PRAGMA foreign_keys = on\r\n";
 
@@ -180,6 +191,94 @@ namespace MetadataDBWork
                         transaction.Commit();
                     }
                 }
+            }
+
+            using (var querycomm = conn.CreateCommand())
+            {
+                var artistlist = new List<string>();
+                var albumartistlist = new List<string>();
+
+                querycomm.CommandText = "INSERT INTO Artists (NAME) SELECT DISTINCT Artist FROM MetadataMapView WHERE Artist NOT IN (SELECT Name FROM Artists)";
+                querycomm.ExecuteNonQuery();
+                querycomm.CommandText = "INSERT INTO AlbumArtists (NAME) SELECT DISTINCT AlbumArtist FROM MetadataMapView WHERE AlbumArtist NOT IN (SELECT Name FROM AlbumArtists)";
+                querycomm.ExecuteNonQuery();
+
+                var artistdict = new Dictionary<string, int>();
+                querycomm.CommandText = "SELECT * FROM Artists";
+                using (var reader = querycomm.ExecuteReader())
+                    while (reader.Read())
+                        artistdict.Add(reader.GetString(1), reader.GetInt32(0));
+
+                var albumartistdict = new Dictionary<string, int>();
+                querycomm.CommandText = "SELECT * FROM AlbumArtists";
+                using (var reader = querycomm.ExecuteReader())
+                    while (reader.Read())
+                        albumartistdict.Add(reader.GetString(1), reader.GetInt32(0));
+
+                var toupdate = new List<(int, string, string, string, string)>();
+                querycomm.CommandText = "SELECT ID, Path, Artist, AlbumArtist, Album FROM MetadataMapView WHERE ArtistID IS NULL OR AlbumID IS NULL";
+                using (var reader = querycomm.ExecuteReader())
+                    while (reader.Read())
+                        toupdate.Add((reader.GetInt32(0), reader[1] as string, reader[2] as string, reader[3] as string, reader[4] as string));
+
+                var distinctalbums = toupdate.Select(t => (Path.GetDirectoryName(t.Item2), t.Item4, t.Item5)).Distinct().ToDictionary(da => da, da => true);
+                var albumsdict = new Dictionary<(string, string, string), int>();
+                querycomm.CommandText = "SELECT Albums.ID, Albums.Path, AlbumArtists.Name, Albums.Name FROM Albums JOIN AlbumArtists ON Albums.AlbumArtistID = AlbumArtists.ID";
+                using (var reader = querycomm.ExecuteReader())
+                    while (reader.Read())
+                        albumsdict.Add((reader[1] as string, reader[2] as string, reader[3] as string), reader.GetInt32(0));
+
+                var differences = distinctalbums.Keys.Except(albumsdict.Keys).ToArray();
+                if (differences.Length > 0)
+                {
+                    using (var transaction = conn.BeginTransaction())
+                    {
+                        querycomm.CommandText = "INSERT INTO Albums (Name, Path, AlbumArtistID) VALUES (@Name, @Path, @AlbumArtistID)";
+                        var nameparam = querycomm.Parameters.Add("@Name", System.Data.DbType.String);
+                        var pathparam = querycomm.Parameters.Add("@Path", System.Data.DbType.String);
+                        var albumartistidparam = querycomm.Parameters.Add("@AlbumArtistID", System.Data.DbType.String);
+                        foreach (var album in differences)
+                        {
+                            nameparam.Value = album.Item3;
+                            pathparam.Value = album.Item1;
+                            albumartistidparam.Value = albumartistdict[album.Item2];
+                            querycomm.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+                    }
+                }
+
+                albumsdict.Clear();
+                querycomm.CommandText = "SELECT Albums.ID, Albums.Path, AlbumArtists.Name, Albums.Name FROM Albums JOIN AlbumArtists ON Albums.AlbumArtistID = AlbumArtists.ID";
+                using (var reader = querycomm.ExecuteReader())
+                    while (reader.Read())
+                        albumsdict.Add((reader[1] as string, reader[2] as string, reader[3] as string), reader.GetInt32(0));
+
+                if (toupdate.Count > 0)
+                {
+                    querycomm.CommandText = "UPDATE Files SET ArtistID = @ArtistID, AlbumID = @AlbumID WHERE ID = @ID";
+                    querycomm.Parameters.Clear();
+                    var artistidparam = querycomm.Parameters.Add("@ArtistID", System.Data.DbType.String);
+                    var albumidparam = querycomm.Parameters.Add("@AlbumID", System.Data.DbType.String);
+                    var idparam = querycomm.Parameters.Add("@ID", System.Data.DbType.String);
+                    using (var transaction = conn.BeginTransaction())
+                    {
+                        foreach (var u in toupdate)
+                        {
+                            idparam.Value = u.Item1;
+                            artistidparam.Value = artistdict[u.Item3];
+                            albumidparam.Value = albumsdict[(Path.GetDirectoryName(u.Item2), u.Item4, u.Item5)];
+                            querycomm.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+
+                    }
+                }
+
+                    Console.WriteLine();                    
+
+
+
             }
 
             Console.WriteLine();
